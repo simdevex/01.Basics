c7f7e98ac9e6e9513751d007b097c5bd38f4abf5
simdevex <sim.devex@gmail.com>
2022-02-22 21:53:31 +1100

    Initial Revision

---
+++ __numberOfDivsorsGivenInteger.py
@@ -0,0 +1,12 @@
+'''
+Python program to find the number of divisors of a given integer is even or odd
+'''
+
+def divisor (n):
+    x = len ([i for i in range (1, n+1) if not n % i])
+    return x
+print(divisor(15)) 
+print(divisor(15))
+print(divisor(9))
+print(divisor(9))
+print(divisor(3))
