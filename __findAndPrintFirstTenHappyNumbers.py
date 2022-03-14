b2d78f6863a712383e2f6cbd417909654913aeac
simdevex <sim.devex@gmail.com>
2022-03-14 19:09:23 +1100

    Modified

--- __findAndPrintFirstTenHappyNumbers.py
+++ __findAndPrintFirstTenHappyNumbers.py
@@ -6,6 +6,7 @@ of the squares of its digits, and repeat the process until the number
 equals 1 (where it will stay), or it loops endlessly in a cycle which
 does not include 1. Those numbers for which this process ends in
 1 are happy numbers, while those that do not end in 1 are unhappy numbers.
+
 Write a Python program to find and print the first 10 happy numbers.
 
 Sample Input:
