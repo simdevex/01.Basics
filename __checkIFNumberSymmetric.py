247e72ab7dba979c65733b1f63d85016470725bf
simdevex <sim.devex@gmail.com>
2022-03-17 20:49:16 +1100

    Initial Revision

---
+++ __checkIFNumberSymmetric.py
@@ -0,0 +1,23 @@
+'''
+Python program to test whether a given number is symmetrical or not. 
+A number is symmetrical when it is equal of its reverse.
+Sample Input:
+(121)
+(0)
+(122)
+(990099)
+Sample Output:
+True
+True
+False
+True
+
+'''
+
+def is_symmetrical_num(n):
+      return str(n) == str(n)[::-1]
+
+print(is_symmetrical_num(121))
+print(is_symmetrical_num(0))
+print(is_symmetrical_num(122))
+print(is_symmetrical_num(990099))
