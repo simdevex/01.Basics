a702a3f97475efb12baae95b85206c997211e59a
simdevex <sim.devex@gmail.com>
2022-03-25 05:12:41 +1100

    Initial Revision

---
+++ __findNameOfOldestStudents.py
@@ -0,0 +1,13 @@
+'''
+Python program to find the name of the oldest student from a given dictionary containing the names and ages of a group of students.
+
+Sample Input:
+{"Bernita Ahner": 12, "Kristie Marsico": 11, "Sara Pardee": 14,
+"Fallon Fabiano": 11, "Nidia Dominique": 15}
+{"Nilda Woodside": 12, "Jackelyn Pineda": 12.2, "Sofia Park": 12.4,
+"Joannie Archibald": 12.6, "Becki Saunder": 12.7}
+Sample Output:
+Nidia Dominique
+Becki Saunder
+
+'''
