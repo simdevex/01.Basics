8e2cdba3241e17f2f9d292e45e54057f2df3ea95
simdevex <sim.devex@gmail.com>
2022-04-04 06:57:37 +1000

    Initial  Revision

---
+++ __checkIfStringContainsConsecutive.py
@@ -0,0 +1,23 @@
+'''
+Write a Python program to check if a given string contains two
+similar consecutive letters. 
+Original string: PHP
+Check for consecutive similar letters! False
+Original string: PHHP
+Check for consecutive similar letters! True
+Original string: PHPP
+Check for consecutive similar letters! True
+'''
+def test(str1):
+    #Check if any of the items in a list are True:
+    return any(c1 == c2 for c1, c2 in zip(str1, str1[1:]))
+
+str = "PHP"
+print("Original string: ",str)
+print("Check for consecutive similar letters! ",test(str))
+str = "PHHP"
+print("\nOriginal string: ",str)
+print("Check for consecutive similar letters! ",test(str))
+str = "PHPP"
+print("\nOriginal string: ",str)
+print("Check for consecutive similar letters! ",test(str))
