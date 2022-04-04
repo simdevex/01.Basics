3998a371ead9e3d73aa6501af08ddecc8ce9a606
simdevex <sim.devex@gmail.com>
2022-04-05 05:42:19 +1000

    Initial Revision

--- __removeAllVowel.py
+++ __removeAllVowel.py
@@ -1,10 +1,26 @@
 '''
-Write a Python program to remove all vowels from a given string. 
+Python program to remove all vowels from a given string. 
 Original string: Python
 After removing all the vowels from the said string: Pythn
 Original string: C Sharp
 After removing all the vowels from the said string: C Shrp
 Original string: JavaScript
 After removing all the vowels from the said string: JvScrpt
-
 '''
+
+#using regular expressions
+import re
+def test(text):
+    return re.sub(r'[aeiou]+', '', text, flags=re.IGNORECASE)
+
+text = "Python";
+print("Original string:",text)
+print("After removing all the vowels from the said string: " + test(text))
+
+text = "C Sharp"
+print("\nOriginal string:",text)
+print("After removing all the vowels from the said string: " + test(text))
+
+text = "JavaScript"
+print("\nOriginal string:",text)
+print("After removing all the vowels from the said string: " + test(text))
