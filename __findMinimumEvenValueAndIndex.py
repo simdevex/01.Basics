e7266ca45202ea3d912207858ec1e45e852c3f9e
simdevex <sim.devex@gmail.com>
2023-01-18 21:11:49 +1100

    Initial Revision

---
+++ __findMinimumEvenValueAndIndex.py
@@ -0,0 +1,23 @@
+'''
+Given an array of numbers representing a branch on a binary
+tree, write a Python program to find the minimum even value and its
+index. In the case of a tie, return the smallest index. If there are no
+even numbers, the answer is []. Go to the editor
+Input:
+[1, 9, 4, 6, 10, 11, 14, 8]
+Output:
+Minimum even value and its index of the said array of numbers:
+[4, 2]
+Input:
+[1, 7, 4, 4, 9, 2]
+Output:
+Minimum even value and its index of the said array of numbers:
+[2, 5]
+Input:
+[1, 7, 7, 5, 9]
+Output:
+Minimum even value and its index of the said array of numbers:
+[]
+Click me to see the sample solution
+
+'''
